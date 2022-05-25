9120ce51fb22e5b6476f0405962f04fe484d594e
Sarmad Raees Khan s294560 <s294560@studenti.polito.it>
2022-05-25 12:16:45 +0500

    corrections for SKUItem_service.dbreal.test.js

--- code/server/unit_test/SKUItem_service.dbreal.test.js
+++ code/server/unit_test/SKUItem_service.dbreal.test.js
@@ -1,4 +1,5 @@
 const SKUItem = require('../components/SKUItem');
+const SKU = require('../components/SKU')
 const SKUItemService = require('../services/SKUItem_service');
 
 const skuItem_dao = require('../database/SKUItem_DAO')
@@ -42,12 +43,14 @@ describe("get SKU Items", () => {
 describe("set SKU Items", () => {
   beforeEach(async () => {
     await skuItems_dao.deleteSKUItem();
+    await sku_dao.deleteSKUData()
     await skuItems_dao.insertSKU(new SKUItem("12345678901234567890123456789014", 1, 0, "2021/11/29 12:30"));
     await skuItems_dao.insertSKU(new SKUItem("12345678901234567890123456789015", 2, 1, "2021/11/30 12:31"));
+    await sku_dao.insertSKU(new SKU (1, "first sku", 60, 20, 10.99, "notes: ...", 50, "800234543412"));
   });
 
   test('new SKU Item', async () => {
-    const SKUItem3 = new SKUItem("12345678901234567890123456789016", 3, 2, "2021/12/29 12:32")
+    const SKUItem3 = new SKUItem("12345678901234567890123456789016", 1, 2, "2021/12/29 12:32")
     const SKUItem4 = new SKUItem("12345678901234567890123456789017", 4, 3, "2021/12/30 12:33")
 
     let res = await SKUItem_service.createSKUItem(SKUItem3.RFID, SKUItem3.SKUID, SKUItem3.available, SKUItem3.dateOfStock);
@@ -62,25 +65,25 @@ describe("set SKU Items", () => {
   })
 
   test('update SKU Item', async () => {
-    const SKUItem1 = new new SKUItem("12345678901234567890123456789024", 4, 1, "2021/11/29 12:30")
-    const SKUItem2 = new new SKUItem("12345678901234567890123456789025", 5, 2, "2021/11/29 12:30")
-    const SKUItem5 = new new SKUItem("12345678901234567890123456789066", 6, 3, "2021/11/29 12:30")
+    const SKUItem1 = new new SKUItem("12345678901234567890123456789024", 1, 2, "2021/11/29 12:30")
+    const SKUItem2 = new new SKUItem("12345678901234567890123456789025", 2, 2, "2021/11/29 12:30")
+    const SKUItem5 = new new SKUItem("12345678901234567890123456789066", 1, 3, "2021/11/29 12:30")
 
-    let res = await SKU_service.updateSKU(SKUItem1.RFID, SKUItem1.SKUID, SKUItem1.available, SKUItem1.dateOfStock)
+    let res = await SKUItem_service.updateSKUItem(SKUItem1.RFID, SKUItem1.SKUID, SKUItem1.available, SKUItem1.dateOfStock)
     expect(res.status).toEqual(200);
-    res = await SKU_service.getSKUItemsBySKUID(SKU1.SKUID);
+    res = await SKUItem_service.getSKUItemsBySKUID(SKU1.SKUID);
     expect(res).toEqual(SKUItem1)
     
-    res = await SKU_service.updateSKU(SKUItem2.RFID, SKUItem2.SKUID, SKUItem2.available, SKUItem2.dateOfStock)
+    res = await SKUItem_service.updateSKUItem(SKUItem2.RFID, SKUItem2.SKUID, SKUItem2.available, SKUItem2.dateOfStock)
     expect(res.status).toEqual(404);
     expect(res.body).toEqual("SKU Item not found")
-    res = await SKU_service.getSKUItemsBySKUID(SKU2.SKUID);
+    res = await SKUItem_service.getSKUItemsBySKUID(SKU2.SKUID);
     expect(res).toEqual({})
 
-    res = await SKU_service.updateSKU(SKUItem5.RFID, SKUItem5.SKUID, SKUItem5.available, SKUItem5.dateOfStock)
+    res = await SKUItem_service.updateSKUItem(SKUItem5.RFID, SKUItem5.SKUID, SKUItem5.available, SKUItem5.dateOfStock)
     expect(res.status).toEqual(404);
-    expect(res.body).toEqual("SKU Item not found")
-    res = await SKU_service.getSKUItemsBySKUID(SKU5.SKUID);
+    expect(res.body).toEqual("ID not found")
+    res = await SKUItem_service.getSKUItemsBySKUID(SKU5.SKUID);
     expect(res).toEqual()
   })
 })
@@ -91,10 +94,10 @@ describe("delete SKU Item", () => {
     await skuItem_dao.insertSKUItem(new SKUItem("12345678901234567890123456789016", 1, 2, "2021/12/29 12:32"));
   });
   test('delete SKU', async () => {
-    const SKUID = 1;
-    let res = await SKU_service.deleteSKU(SKUID);
+    const RFID = "12345678901234567890123456789016";
+    let res = await SKUItem_service.deleteSKUItem(RFID);
     expect(res).toEqual(true);
-    res = await SKU_service.getSKUbyId(SKUID);
+    res = await SKUItem_service.getSKUItemByRFID(RFID);
     expect(res).toBeNull();
   });
 })
